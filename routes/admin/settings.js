b1dcb9d65d5d56b12968a590443a95cb12382fa7
lizhipeng <845007771@qq.com>
2019-01-30 09:42:14 +0800

    提交

--- routes/admin/settings.js
+++ routes/admin/settings.js
@@ -1,24 +1,19 @@
 const express=require("express");
 const router=express.Router();
 const pool=require("../../pool.js");
-router.post("/update",(req,res)=>{
-    var $aid=req.body.aid;
-    var $appName=req.body.appName;
-    var $apiUrl=req.body.apiUrl;
-    var $addminUrl=req.body.addminUrl;
-    var $appUrl=req.body.appUrl;
-    var $icp=req.body.icp;
-    var $copyright=req.body.copyright;
+module.exports = router;
 
-    var sql="UPDATE xfn_settings SET aid=?,appName=?,apiUrl=?,addminUrl=?,appUrl=?,icp=?,copyright=? WHERE aid=?";
-    pool.query(sql,[$appName,$apiUrl,$addminUrl,$appUrl,$icp,$copyright,$aid],(err,result)=>{
-        if(err){
-            throw err;
-        }
-        if(result.affectedRows>0){
-            res.send({code:200,msg:"修改成功"});
-        }else{
-            res.send({code:301,msg:"修改失败"});
-        }
+
+router.get('/',(req,res)=>{
+    pool.query('SELECT * FROM xfn_settings LIMIT 1',(err,result)=>{
+        if (err) throw err;
+        res.send(result[0])
+    })
+})
+
+router.put('/',(req,res)=>{
+    pool.query('UPDATE  xfn_settings SET ?', req.body,(err,result)=>{
+        if (err) throw err;
+        res.send({code:200, msg:'settings updated succ'})
     })
 })
