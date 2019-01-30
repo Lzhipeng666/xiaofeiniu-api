5ff6b34f850841ffe0ed2a20875023cc30c782ba
lizhipeng <845007771@qq.com>
2019-01-30 10:22:47 +0800

    api 完成

--- routes/admin/table.js
+++ routes/admin/table.js
@@ -1,37 +1,23 @@
-const express=require("express");
-const router=express.Router();
-const pool=require("../../pool.js");
-router.get("/list",(req,res)=>{
-    var sql="SELECT * FROM xfn_table";
-    pool.query(sql,[$aname,$apwd],(err,result)=>{
-        if(err){
-            throw err;
-        }
-        res.send(result);
-    })
-})
-router.get("/detail",(req,res)=>{
-    var $tid=req.body.tid;
-    var sql="SELECT * FROM xfn_table_detail WHERE tid=?";
-    pool.query(sql,[$tid],(err,result)=>{
-        if(err){
-            throw err;
-        }
-        res.send(result);
-    })
-})
-router.post("/update",(req,res)=>{
-    var $tid=req.body.tid;
-    var $status=req.body.status;
-    var sql="UPDATE xfn_table SET status=? WHERE tid=?";
-    pool.query(sql,[$status,$tid],(err,result)=>{
-        if(err){
-            throw err;
-        }
-        if(result.affectedRows>0){
-            res.send({code:200,msg:"修改成功"});
-        }else{
-            res.send({code:301,msg:"修改失败"});
-        }
-    })
+/*
+*桌台相关的路由器
+*/
+const express = require('express');
+const pool = require('../../pool');
+var router = express.Router();
+module.exports = router;
+
+/*
+*GET  /admin/table
+*获取所有的桌台信息
+*返回数据：
+*   [
+*     {tid:xxx, tname:'xxx', status:''},
+*     ...
+*   ]
+*/
+router.get('/', (req, res)=>{
+  pool.query('SELECT * FROM xfn_table ORDER BY tid', (err, result)=>{
+    if(err)throw err;
+    res.send(result);
+  })
 })
